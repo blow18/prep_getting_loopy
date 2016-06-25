0960da4e2475f5a13caa62aedabb82989391ce87
blow18 <blow18@punahou.edu>
2016-06-24 14:22:54 -1000

    Finished presidents

--- loops.js
+++ loops.js
@@ -1,1 +1,12 @@
 // Do your work here
+
+function runPresidents() {
+	var presidents = ["Washington", "Adams", "Jefferson", "Madison", "Monroe"];
+
+	for (i in presidents) {
+		console.log("The value of i is: " + i);
+		console.log("The value at Index is: " + presidents[i]);
+	}
+}
+
+runPresidents();
